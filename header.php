25e87c50f8dff7b9d56578bcb1feba1b610e19e4
Clarisa Rodríguez <clarisacandia0108@gmail.com>
2024-09-10 19:17:34 -0300

    Separe el header en un archivo aparte

---
+++ header.php
@@ -0,0 +1,32 @@
+<header>
+    <div class="d-flex flex-column flex-md-row justify-content-between align-items-center">
+        <img src="img/logo-pokedex.png" alt="logo" style="width: 50px; height: 50px">
+        <h1 class="my-2 my-md-0">Pokedex</h1>
+        <?php
+        session_start();
+        if(!isset($_SESSION['logueado'])){
+            echo '<form  method="post" action="manejoDeSesion.php">';
+            echo '<div class="d-flex flex-column flex-md-row align-items-center">';
+            echo '<div class="mb-2 mb-md-0 mr-md-2">';
+            echo  '<label for="usuario" class="sr-only">Usuario</label>';
+            echo '<input name="usuario" type="text" class="form-control" id="usuario" placeholder="usuario" required>';
+            echo' </div>';
+            echo '<div class="mb-2 mb-md-0 mr-md-2">';
+            echo '<label for="contrasenia" class="sr-only">Contraseña</label>';
+            echo '<input name="contrasenia" type="password" class="form-control" id="contrasenia" placeholder="contraseña" required>';
+            echo '</div>';
+            echo '<div>';
+            echo '<button type="submit" class="btn btn-primary">Ingresar</button>';
+            echo '</div>';
+            echo '</div>';
+            if(isset($_GET['error'])){
+                $error = $_GET['error'];
+                echo '<div class="alert alert-danger alert-dismissible fade show mt-2" role="alert">' . $error . '</div>';
+            }
+            echo '</form>';
+        }else{
+            echo '<h3>' . $_SESSION['usuario'] . '</h3>';
+        }
+        ?>
+    </div>
+</header>
