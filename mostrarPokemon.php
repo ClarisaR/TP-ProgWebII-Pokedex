f94ff09337779bc4b44e8c62d55db94ffffa5366
Lautaro Desouches <lautarodesouches@outlook.com>
2024-09-19 11:07:58 -0300

    Base mostrar pokemon

---
+++ mostrarPokemon.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokedex</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container-fluid d-flex flex-column min-vh-100">
+    <?php include_once('components/header.php'); ?>
+    <main class="flex-grow-1 row">
+        <div class="col-12 col-md-6 p-5 bg-danger"></div>
+        <div class="col-12 col-md-6 p-5 bg-dark"></div>
+    </main>
+    <?php include_once('components/footer.php'); ?>
+</div>
+</body>
+</html>
